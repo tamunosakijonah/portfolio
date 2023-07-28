fa5ecbe21fdc8b6057c64c046cff6decd818cb3d
Tamunosaki <tamunosakijonah@gmail.com>
2023-07-28 04:22:38 +0100

    second

--- src/header.js
+++ src/header.js
@@ -3,8 +3,8 @@ import img2 from './images/icon-hamburger.svg'
 function Header(){
     return(
         <div className='apart'>
-            <div><img src={img1}></img></div>
-            <div><img src={img2}></img></div>
+            <div><img src={img1} alt='img'></img></div>
+            <div><img src={img2} alt='img'></img></div>
 
         </div>
     )
